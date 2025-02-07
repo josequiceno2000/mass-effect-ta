3962f771a00dd7ab3dddb2943ef39d6e87f8532f
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-02-07 14:25:45 -0600

    Append farewell message if user quits

--- mass-effect.cpp
+++ mass-effect.cpp
@@ -3,6 +3,7 @@
 #include <string>
 
 int main() {
+    // Welcome Message
     std::ifstream file("ascii-art/title-card.txt"); 
     std::string line;
 
@@ -12,5 +13,26 @@ int main() {
 
     file.close();
 
-    return 0;
+
+    std::cout << "Welcome to the Mass Effect Text Adventure.\n";
+    std::cout << "To quit at any time, simply type 'q' (lowercase).\n";
+
+    std::string user_input;
+    std::cin >> user_input;
+
+    // User quitting condition
+    if (user_input == "q") {
+        std::ifstream file("ascii-art/farewell.txt"); 
+        std::string line;
+
+        while (std::getline(file, line)) {
+            std::cout << line << std::endl;
+        }
+
+    file.close();
+
+        return 0;
+    }
+
+    
 }
