7a5510e48429321449fb3f1b15a8f62d3ae1525c
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-02-07 14:51:54 -0600

    Add Chapter 0 to storyline

--- mass-effect.cpp
+++ mass-effect.cpp
@@ -13,7 +13,6 @@ int main() {
 
     file.close();
 
-
     std::cout << "Welcome to the Mass Effect Text Adventure.\n";
     std::cout << "To quit at any time, simply type 'q' (lowercase).\n";
 
@@ -29,10 +28,23 @@ int main() {
             std::cout << line << std::endl;
         }
 
-    file.close();
+        file.close();
 
         return 0;
     }
+
+    // Chapter 0 
+    std::cout << "\n[CHAPTER 0: MASS EFFECT]\n"
+    "\nIn the year 2148, explorers on Mars discovered the remains of an ancient spacefaring civilization. In the \n" 
+    "decades that followed, these mysterious artifacts revealed startling new technologies, enabling travel \n" 
+    "to the furthest stars. The basis for this incredible technology was a force that \n" 
+    "controlled the very fabric of space and time.\n"
+    "\n"
+    "They called it the greatest discovery in human history.\n"
+    "\n"
+    "The civilizations of the galaxy call it... MASS EFFECT.\n\n";
+
+    return 0;
 
     
 }
