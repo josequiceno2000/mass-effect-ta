220152887e46ac4108b19f6d5b1ccddebc5ce07f
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-02-07 17:11:54 -0600

    Add Tali recruit option + improve UI

--- mass-effect.cpp
+++ mass-effect.cpp
@@ -20,6 +20,16 @@ int main() {
     std::cin >> user_input;
 
     // User quitting condition
+    while (user_input != "q" && user_input != "p") {
+
+        std::cout << "\nNot a valid choice.\n"
+        "Please enter:\n"
+        "'p'\n"
+        "or 'q'\n";
+        
+        std::cin >> user_input;
+
+    }
     if (user_input == "q") {
         std::ifstream file("ascii-art/farewell.txt"); 
         std::string line;
@@ -46,6 +56,37 @@ int main() {
     "They called it the greatest discovery in human history.\n"
     "\n"
     "The civilizations of the galaxy call it... MASS EFFECT.\n\n";
+
+    // User quitting condition
+    std::cout << "Enter 'p' to continue, or 'q' to quit.\n";
+    std::cin >> user_input;
+
+    
+    while (user_input != "q" && user_input != "p") {
+
+        std::cout << "\nNot a valid choice.\n"
+        "Please enter:\n"
+        "'p'\n"
+        "or 'q'\n";
+        
+        std::cin >> user_input;
+
+    }
+    if (user_input == "q") {
+        std::ifstream file("ascii-art/farewell.txt"); 
+        std::string line;
+
+        while (std::getline(file, line)) {
+            std::cout << line << std::endl;
+        }
+
+        file.close();
+
+        return 0;
+    }
+    else if (user_input == "p") {
+        ;
+    }
 
     // Chapter 1
     // Choice 1: Recruit Garrus
@@ -94,8 +135,8 @@ int main() {
         "====== SQUAD ROSTER ======\n"
         "Ashley Williams\n"
         "Kaidan Alenko\n"
-        "Tali'Zorah nar Rayya\n"
         "Garrus Vakarian\n\n"
+        "==========================\n"
         "---------------------------\n\n";
     }
     else {
@@ -104,13 +145,46 @@ int main() {
         "\n"
         "====== SQUAD ROSTER ======\n"
         "Ashley Williams\n"
-        "Kaidan Alenko\n"
-        "Tali'Zorah nar Rayya\n\n"
+        "Kaidan Alenko\n\n"
+        "==========================\n"
         "---------------------------\n\n";
     }
 
+    // User quitting condition
+    std::cout << "Enter 'p' to continue, or 'q' to quit.\n";
+    std::cin >> user_input;
+
+    
+    while (user_input != "q" && user_input != "p") {
+
+        std::cout << "\nNot a valid choice.\n"
+        "Please enter:\n"
+        "'p'\n"
+        "or 'q'\n";
+        
+        std::cin >> user_input;
+
+    }
+    if (user_input == "q") {
+        std::ifstream file("ascii-art/farewell.txt"); 
+        std::string line;
+
+        while (std::getline(file, line)) {
+            std::cout << line << std::endl;
+        }
+
+        file.close();
+
+        return 0;
+    }
+    else if (user_input == "p") {
+        ;
+    }
+
+
     // Choice 2: Recruit Wrex
-    std::cout << "Next is the unlikely, unwieldy, and completely unstopable URDNOT WREX.\n"
+    std::cout << "\n\n---------------------------\n\n"
+    "Next is the unlikely, unwieldy, and completely unstopable URDNOT WREX.\n"
     "\nWrex  is a Krogan bounty hunter working for a crime lord on the Citadel. Doesn't seem to interested in much besides payday. \n"
     "His race was genetically sterilized by the Salarians, leading to devastating effects for his people. Kind of explains the attitude problem. \n"
     "Despire a rough exterior, there seems to be some kind of code that guides him. Hopefully. \n"
@@ -130,7 +204,7 @@ int main() {
         "'2'\n"
         "or 'q'\n";
         
-        std::cin >> user_choice_1;
+        std::cin >> user_choice_2;
     }
 
     if (user_choice_2 == "q") {
@@ -142,28 +216,247 @@ int main() {
         return 0;
     }
     else if (user_choice_2 == "1") {
-        std::cout << "\n"
-        "You have successfully recruited Wrex to the ship!\n"
-        "He hangs out in the cargo bay, always willing to talk about getting a cure for his people.\n"
-        "And I do mean always.\n"
-        "\n"
-        "====== SQUAD ROSTER ======\n"
-        "Ashley Williams\n"
-        "Kaidan Alenko\n"
-        "Tali'Zorah nar Rayya\n"
-        "Garrus Vakarian\n"
-        "Urdnot Wrex\n";
+        if (user_choice_1 == "1") { 
+            std::cout << "\n"
+            "You have successfully recruited Wrex to the ship!\n"
+            "He hangs out in the cargo bay, always willing to talk about getting a cure for his people.\n"
+            "And I do mean always.\n"
+            "\n"
+            "====== SQUAD ROSTER ======\n"
+            "Ashley Williams\n"
+            "Kaidan Alenko\n"
+            "Garrus Vakarian\n"
+            "Urdnot Wrex\n"
+            "==========================\n"
+            "---------------------------\n\n";;
+        }
+        else if (user_choice_1 == "2") {
+            std::cout << "\n"
+            "You have successfully recruited Wrex to the ship!\n"
+            "He hangs out in the cargo bay, always willing to talk about getting a cure for his people.\n"
+            "And I do mean always.\n"
+            "\n"
+            "====== SQUAD ROSTER ======\n"
+            "Ashley Williams\n"
+            "Kaidan Alenko\n"
+            "Urdnot Wrex\n"
+            "==========================\n"
+            "---------------------------\n\n";;
+        }        
     }
     else {
-        std::cout << "\n"
-        "You leave Wrex. He's too dangerous.\n"
-        "\n"
-        "====== SQUAD ROSTER ======\n"
-        "Ashley Williams\n"
-        "Kaidan Alenko\n"
-        "Tali'Zorah nar Rayya\n"
-        "Garrus Vakarian\n";
+        if (user_choice_1 == "1") {
+            std::cout << "\n"
+            "You leave Wrex. He's too dangerous.\n"
+            "\n"
+            "====== SQUAD ROSTER ======\n"
+            "Ashley Williams\n"
+            "Kaidan Alenko\n"
+            "Tali'Zorah nar Rayya\n"
+            "Garrus Vakarian\n"
+            "==========================\n"
+            "---------------------------\n\n";;
+        }
+        else if (user_choice_1 == "2") {
+            std::cout << "\n"
+            "You leave Wrex. He's too dangerous.\n"
+            "\n"
+            "====== SQUAD ROSTER ======\n"
+            "Ashley Williams\n"
+            "Kaidan Alenko\n"
+            "Tali'Zorah nar Rayya\n"
+            "==========================\n"
+            "---------------------------\n\n";;
+        }     
     }
 
+    // User quitting condition
+    std::cout << "Enter 'p' to continue, or 'q' to quit.\n";
+    std::cin >> user_input;
+
+    
+    while (user_input != "q" && user_input != "p") {
+
+        std::cout << "\nNot a valid choice.\n"
+        "Please enter:\n"
+        "'p'\n"
+        "or 'q'\n";
+        
+        std::cin >> user_input;
+
+    }
+    if (user_input == "q") {
+        std::ifstream file("ascii-art/farewell.txt"); 
+        std::string line;
+
+        while (std::getline(file, line)) {
+            std::cout << line << std::endl;
+        }
+
+        file.close();
+
+        return 0;
+    }
+    else if (user_input == "p") {
+        ;
+    }
+
+    // Choice 3: Recruit Tali
+    std::cout << "\n\n---------------------------\n\n"
+    "Finally, TALI'ZORAH NAR RAYYA.\n"
+    "\nTali is a Quarian nomad looking to accomplish something worthy of her Fleet's honor. It must be some task of Quarian significance. \n"
+    "She is evidently a technical genious. However, she is also a known pick-pocket and thief. Can you really trust someone like that? \n"
+    "I suppose that depends on whether she considers you an ally or an enemy. \n"
+    "\n"
+    "[MAKE A DECISION]\n"
+    "1] Recruit Tali\n"
+    "2] Ignore Tali\n";
+
+    std::string user_choice_3;
+    std::cin >> user_choice_3;
+
+    while (user_choice_3 != "q" && user_choice_3 != "1" && user_choice_3 != "2") {
+
+        std::cout << "\nNot a valid choice.\n"
+        "Please enter:\n"
+        "'1'\n"
+        "'2'\n"
+        "or 'q'\n";
+        
+        std::cin >> user_choice_3;
+    }
+
+    if (user_choice_3 == "q") {
+        while (std::getline(file, line)) {
+            std::cout << line << std::endl;
+        }
+
+        file.close();
+        return 0;
+    }
+    else if (user_choice_3 == "1") {
+        if (user_choice_2 == "1") { 
+            if (user_choice_1 == "1") {
+                std::cout << "\n"
+                "You have successfully recruited Tali to the ship!\n"
+                "She's (unsurprisingly) marvelling at the mass effect core which powers the Normandy.\n"
+                "I'm sure she will be a fine addition to the team.\n"
+                "\n"
+                "====== SQUAD ROSTER ======\n"
+                "Ashley Williams\n"
+                "Kaidan Alenko\n"
+                "Garrus Vakarian\n"
+                "Urdnot Wrex\n"
+                "Tali'Zorah nar Rayya\n"
+                "==========================\n"
+                "---------------------------\n\n";;
+            }
+            else if (user_choice_1 == "2") {
+                std::cout << "\n"
+                "You have successfully recruited Tali to the ship!\n"
+                "She's (unsurprisingly) marvelling at the mass effect core which powers the Normandy.\n"
+                "I'm sure she will be a fine addition to the team.\n"
+                "\n"
+                "====== SQUAD ROSTER ======\n"
+                "Ashley Williams\n"
+                "Kaidan Alenko\n"
+                "Urdnot Wrex\n"
+                "Tali'Zorah nar Rayya\n"
+                "==========================\n"
+                "---------------------------\n\n";;
+            }
+        }
+        else if (user_choice_2 == "2") {
+            if (user_choice_1 == "1") {
+                std::cout << "\n"
+                "You have successfully recruited Tali to the ship!\n"
+                "She's (unsurprisingly) marvelling at the mass effect core which powers the Normandy.\n"
+                "I'm sure she will be a fine addition to the team.\n"
+                "\n"
+                "====== SQUAD ROSTER ======\n"
+                "Ashley Williams\n"
+                "Kaidan Alenko\n"
+                "Garrus Vakarian\n"
+                "Tali'Zorah nar Rayya\n"
+                "==========================\n"
+                "---------------------------\n\n";;
+            }
+            else if (user_choice_1 == "2") {
+                std::cout << "\n"
+                "You have successfully recruited Tali to the ship!\n"
+                "She's (unsurprisingly) marvelling at the mass effect core which powers the Normandy.\n"
+                "I'm sure she will be a fine addition to the team.\n"
+                "\n"
+                "====== SQUAD ROSTER ======\n"
+                "Ashley Williams\n"
+                "Kaidan Alenko\n"
+                "Tali'Zorah nar Rayya\n"
+                "==========================\n"
+                "---------------------------\n\n";;
+            }
+        }
+        
+    }
+    else if (user_choice_3 == "2") {
+        if (user_choice_2 == "1") { 
+            if (user_choice_1 == "1") {
+                std::cout << "\n"
+                "You have successfully recruited Tali to the ship!\n"
+                "She's (unsurprisingly) marvelling at the mass effect core which powers the Normandy.\n"
+                "I'm sure she will be a fine addition to the team.\n"
+                "\n"
+                "====== SQUAD ROSTER ======\n"
+                "Ashley Williams\n"
+                "Kaidan Alenko\n"
+                "Garrus Vakarian\n"
+                "Urdnot Wrex\n"
+                "==========================\n"
+                "---------------------------\n\n";;
+            }
+            else if (user_choice_1 == "2") {
+                std::cout << "\n"
+                "You have successfully recruited Tali to the ship!\n"
+                "She's (unsurprisingly) marvelling at the mass effect core which powers the Normandy.\n"
+                "I'm sure she will be a fine addition to the team.\n"
+                "\n"
+                "====== SQUAD ROSTER ======\n"
+                "Ashley Williams\n"
+                "Kaidan Alenko\n"
+                "Urdnot Wrex\n"
+                "==========================\n"
+                "---------------------------\n\n";;
+            }
+        }
+        else if (user_choice_2 == "2") {
+            if (user_choice_1 == "1") {
+                std::cout << "\n"
+                "You have successfully recruited Tali to the ship!\n"
+                "She's (unsurprisingly) marvelling at the mass effect core which powers the Normandy.\n"
+                "I'm sure she will be a fine addition to the team.\n"
+                "\n"
+                "====== SQUAD ROSTER ======\n"
+                "Ashley Williams\n"
+                "Kaidan Alenko\n"
+                "Garrus Vakarian\n"
+                "==========================\n"
+                "---------------------------\n\n";;
+            }
+            else if (user_choice_1 == "2") {
+                std::cout << "\n"
+                "You have successfully recruited Tali to the ship!\n"
+                "She's (unsurprisingly) marvelling at the mass effect core which powers the Normandy.\n"
+                "I'm sure she will be a fine addition to the team.\n"
+                "\n"
+                "====== SQUAD ROSTER ======\n"
+                "Ashley Williams\n"
+                "Kaidan Alenko\n"
+                "==========================\n\n"
+                "---------------------------\n\n";;
+            }
+        }
+        
+    }
+
+    
     
 }
